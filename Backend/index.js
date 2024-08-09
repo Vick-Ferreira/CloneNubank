ae8bf6fe5621c4c798c60796aa538940cca6e3fe
Vitoria Ferreira <vitoriaferreirap06@gmail.com>
2024-08-09 13:16:22 -0300

    upgrade

---
+++ Backend/index.js
@@ -0,0 +1,38 @@
+const express = require('express');
+const app = express();
+const cors = require('cors');
+const mongoose = require('mongoose');
+require('dotenv').config();
+const path = require('path');
+
+const port = process.env.PORT || 8080;
+
+app.use(express.json());
+app.use(cors());
+
+// Importar e usar as rotas
+const carroselCardsRouter = require('./routes/carroselCardsRoutes');
+app.use(carroselCardsRouter);
+
+// Servir arquivos estáticos
+const frontendPath = path.join(__dirname, '../Frontend');
+app.use(express.static(frontendPath));
+
+
+
+// Conectar com MongoDB
+mongoose.connect(process.env.MONGODB_URI)
+    .then(() => {
+        console.log('Conectado ao MongoDB');
+    })
+    .catch((error) => {
+        console.error('Erro ao conectar ao MongoDB', error);
+    });
+
+
+    
+
+// Inicialização do servidor
+app.listen(port, () => {
+    console.log(`Servidor está rodando na porta: ${port}`);
+});
